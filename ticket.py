f0163c1d3027fcf3f1a3429a341dba0fd487cb8c
jihyunna <skwlgus106@naver.com>
2023-05-28 21:54:57 +0900

    ✨ #1 좌석·일시·장소 정보만 추출해서 반환

--- ticket.py
+++ ticket.py
@@ -1,9 +1,12 @@
 import mysql.connector
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import cv2
 import pytesseract
+import requests
 from config import DATABASE_CONFIG
 import os
+import re
+
 
 # Tesseract 바이너리 파일 경로 설정
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
@@ -24,6 +27,7 @@ def recognize_text(image_path):
     text = pytesseract.image_to_string(gray, lang='kor+eng')
     return text
 
+
 def get_next_img_name():
     i = 1
     while True:
@@ -32,6 +36,35 @@ def get_next_img_name():
             return img_name
         i += 1
 
+
+def extract_text(text, keyword, pattern):
+    match = re.search(pattern, text)
+    if match:
+        return match.group(1).strip()
+    return None
+
+def extract_ticket_info(text):
+    seat = None
+    location = None
+    date_time = None
+
+    # 좌석 정보 추출
+    seat_match = re.search(r"(\d+)[^\d]+(\d+)[^\d]+(\d+)", text)
+    if seat_match:
+        floor = seat_match.group(1)
+        row = seat_match.group(2)
+        number = seat_match.group(3)
+        seat = f"{floor}층 {row}열 {number}번"
+
+    # 일시 정보 추출
+    date_time_pattern = rf"일 시\s*[:：.]?\s*([\w\s]+)"
+    date_time = extract_text(text, "일 시", date_time_pattern)
+
+    # 장소 정보 추출
+    location_pattern = rf"장 소\s*[:：.]?\s*([^\W\d_]+(?:\s+[^\W\d_]+)*)"
+    location = extract_text(text, "장 소", location_pattern)
+
+    return seat, location, date_time
 
 @app.route('/api/v1/view-review/ticket', methods=['POST'])
 def upload_ticket():
@@ -47,13 +80,24 @@ def upload_ticket():
     # 텍스트 추출
     text = recognize_text(image_path)
 
-    return text
+    # 티켓 정보 추출
+    seat, location, date_time = extract_ticket_info(text)
+
+    response = {
+        '좌석': seat,
+        '장소': location,
+        '일시': date_time
+    }
+
+    return jsonify(response)
+
 
 @app.route('/api/v1/view-review/ticket', methods=['PUT'])
 def edit_ticket():
 
-    text = request.form['text']
-    return text
+    edited_data = request.get_json()
+    return jsonify(edited_data)
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8080, debug=True)
+
